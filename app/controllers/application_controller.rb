d182931c22755b0a247554bc180e2710a5b4705a
Matias Escobar <mescobar@chicagobooth.edu>
2023-04-13 04:34:01 +0000

    Random working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,28 @@ class ApplicationController < ActionController::Base
 
 
   def calculate_square
-    #params = {"results"=>"42"}
+    #ruby brings params = {"results"=>"42"}
 
-    @num = params.fetch("results").to_f
+    @num = params.fetch("results_square").to_f
     @square_of_num = @num ** 2
 
     render({ :template => "calculation_templates/square_results.html.erb" })
+  end
+
+
+  def blank_random_form
+    render({ :template => "calculation_templates/random_form.html.erb" })
+  end
+
+
+  def calculate_random
+#Parameters: {"user_min"=>"1", "user_max"=>"5"}
+
+  @lower = params.fetch("user_min").to_f
+  @upper = params.fetch("user_max").to_f
+  @result_random = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/random_results.html.erb" })
   end
 
 end
