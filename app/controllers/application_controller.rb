ae38e13a2d5d668964ba954023af1d5b281b918a
Matias Escobar <mescobar@chicagobooth.edu>
2023-04-13 04:10:58 +0000

    Square calculator working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_form.html.erb" })
   end
 
+
+  def calculate_square
+    #params = {"results"=>"42"}
+
+    @num = params.fetch("results").to_f
+    @square_of_num = @num ** 2
+
+    render({ :template => "calculation_templates/square_results.html.erb" })
+  end
+
 end
